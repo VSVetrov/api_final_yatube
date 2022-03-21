3060f99079c3dbd9f6dbc4e46ef5da9b10ab900f
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-03-21 06:25:40 +0300

    api_final

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,3 @@
-# TODO:  Напишите свой вариант
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
